a2ed155324205116b675cff391b72996367495cc
DaryPet <d7akkord@gmail.com>
2024-12-25 22:18:51 +0100

    room+2 elems 3d

--- src/app/sections/Hero.jsx
+++ src/app/sections/Hero.jsx
@@ -20,7 +20,7 @@
 // export default Portfolio;
 
 "use client";
-import { Leva } from "leva";
+import { Leva, useControls } from "leva";
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { useMediaQuery } from "react-responsive";
@@ -28,45 +28,55 @@ import { PerspectiveCamera } from "@react-three/drei";
 
 // import Cube from "../components/Cube.jsx";
 // import Rings from "../components/Rings.jsx";
-// import ReactLogo from "../components/ReactLogo.jsx";
+import ReactLogo from "../components/ReactLogo";
 // import Button from "../components/Button.jsx";
 // import Target from "../components/Target.jsx";
 import CanvasLoader from "../components/Loader";
 // import HeroCamera from "../components/HeroCamera.jsx";
 import { calculateSizes } from "../constants/index.js";
 import HackerRoom from "../components/HackerRoom.jsx";
+import Arrow from "../components/Arrow";
+import Target from "../components/Target.jsx";
 
 const Hero = () => {
-  // Use media queries to determine screen size
-  // const isSmall = useMediaQuery({ maxWidth: 440 });
-  // const isMobile = useMediaQuery({ maxWidth: 768 });
-  // const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
+  // const controls = useControls({
+  //   schemaOrFolderName: "HackerRoom",
+  //   settingsOrDepsOrSchema: {
+  //     rotationX: {
+  //       value: 2.5,
+  //       min: -10,
+  //       max: 10,
+  //     },
+  //   },
+  // });
+  const isSmall = useMediaQuery({ maxWidth: 440 });
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
 
-  // const sizes = calculateSizes(isSmall, isMobile, isTablet);
-
+  const sizes = calculateSizes(isSmall, isMobile, isTablet);
   return (
     <section className="min-h-screen w-full flex flex-col relative" id="home">
-      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3 ">
         <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
           {/* Darya Petrenko <span className="waving-hand">👋</span> */}
           Darya Petrenko
         </p>
-        <p className="hero_tag text-gray_gradient">
+        <p className="hero_tag text-gray_gradient ">
           Building Products & Brands
         </p>
       </div>
 
-      <div className="w-full h-full absolute inset-0">
+      <div className="w-full h-full absolute inset-0 my-20">
         <Canvas className="w-full h-full">
           <Suspense fallback={<CanvasLoader />}>
             {/* To hide controller */}
             {/* <Leva hidden />  */}
-            <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+            <PerspectiveCamera makeDefault position={[0, 0, 20]} />
             {/* <HeroCamera isMobile={isMobile}> */}
             <HackerRoom
-            // scale={sizes.deskScale}
-            // position={sizes.deskPosition}
-            // rotation={[0.1, -Math.PI, 0]}
+              scale={sizes.deskScale}
+              position={sizes.deskPosition}
+              rotation={[0.1, -Math.PI, 0]}
             />
             {/* </HeroCamera> */}
             {/* <group>
@@ -74,11 +84,14 @@ const Hero = () => {
               <ReactLogo position={sizes.reactLogoPosition} />
               <Rings position={sizes.ringPosition} />
               <Cube position={sizes.cubePosition} />
+            </group> */}
+            <group>
+              <Target position={sizes.targetPosition} />
+              <Arrow position={sizes.arrowPosition} />
+              <ReactLogo position={sizes.reactLogoPosition} />
             </group>
-
             <ambientLight intensity={1} />
             <directionalLight position={[10, 10, 10]} intensity={0.5} />
-          </Suspense> */}
           </Suspense>
         </Canvas>
       </div>
