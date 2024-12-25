2991ac5a8720bb6af3893649c1a1182e21e656fa
DaryPet <d7akkord@gmail.com>
2024-12-25 17:06:29 +0100

    change settings-next13,react18

--- src/app/sections/Hero.jsx
+++ src/app/sections/Hero.jsx
@@ -1,5 +1,25 @@
-"use client";
+// "use client"; // Указание, что это клиентский компонент
 
+// import { Canvas } from "@react-three/fiber";
+// import { OrbitControls } from "@react-three/drei";
+
+// function Portfolio() {
+//   return (
+//     <Canvas>
+//       <ambientLight />
+//       <pointLight position={[10, 10, 10]} />
+//       <OrbitControls />
+//       <mesh position={[0, 0, 0]}>
+//         <boxGeometry args={[1, 1, 1]} />
+//         <meshStandardMaterial color="orange" />
+//       </mesh>
+//     </Canvas>
+//   );
+// }
+
+// export default Portfolio;
+
+"use client";
 import { Leva } from "leva";
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
@@ -13,23 +33,22 @@ import { PerspectiveCamera } from "@react-three/drei";
 // import Target from "../components/Target.jsx";
 // import CanvasLoader from "../components/Loading.jsx";
 // import HeroCamera from "../components/HeroCamera.jsx";
-import { calculateSizes } from "../constants/index";
-import HackerRoom from "../components/HackerRoom";
-// import { HackerRoom } from "../components/HackerRoom.jsx";
+import { calculateSizes } from "../constants/index.js";
+import HackerRoom from "../components/HackerRoom.jsx";
 
 const Hero = () => {
   // Use media queries to determine screen size
-  const isSmall = useMediaQuery({ maxWidth: 440 });
-  const isMobile = useMediaQuery({ maxWidth: 768 });
-  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
+  // const isSmall = useMediaQuery({ maxWidth: 440 });
+  // const isMobile = useMediaQuery({ maxWidth: 768 });
+  // const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
 
-  const sizes = calculateSizes(isSmall, isMobile, isTablet);
+  // const sizes = calculateSizes(isSmall, isMobile, isTablet);
 
   return (
     <section className="min-h-screen w-full flex flex-col relative" id="home">
       <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
         <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
-          Darya Full stack <span className="waving-hand">👋</span>
+          Hi, I am Adrian <span className="waving-hand">👋</span>
         </p>
         <p className="hero_tag text-gray_gradient">
           Building Products & Brands
@@ -38,20 +57,17 @@ const Hero = () => {
 
       <div className="w-full h-full absolute inset-0">
         <Canvas className="w-full h-full">
-          {/* <Suspense fallback={<CanvasLoader />}> */}
-          {/* To hide controller */}
-          {/* <Leva hidden /> */}
+          {/* <Suspense fallback={<CanvasLoader />}>
+            {/* To hide controller */}
+          {/* <Leva hidden />  */}
           <PerspectiveCamera makeDefault position={[0, 0, 30]} />
-          <HackerRoom />
-
-          {/* <HeroCamera isMobile={isMobile}>
-              <HackerRoom
-                scale={sizes.deskScale}
-                position={sizes.deskPosition}
-                rotation={[0.1, -Math.PI, 0]}
-              />
-            </HeroCamera> */}
-
+          {/* <HeroCamera isMobile={isMobile}> */}
+          <HackerRoom
+          // scale={sizes.deskScale}
+          // position={sizes.deskPosition}
+          // rotation={[0.1, -Math.PI, 0]}
+          />
+          {/* </HeroCamera> */}
           {/* <group>
               <Target position={sizes.targetPosition} />
               <ReactLogo position={sizes.reactLogoPosition} />
@@ -65,7 +81,7 @@ const Hero = () => {
         </Canvas>
       </div>
 
-      <div className="absolute bottom-7 left-0 right-0 w-full z-10 c-space">
+      {/* <div className="absolute bottom-7 left-0 right-0 w-full z-10 c-space">
         <a href="#about" className="w-fit">
           <Button
             name="Let's work together"
@@ -73,7 +89,7 @@ const Hero = () => {
             containerClass="sm:w-fit w-full sm:min-w-96"
           />
         </a>
-      </div>
+      </div> */}
     </section>
   );
 };
