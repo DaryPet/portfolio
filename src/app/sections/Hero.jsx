43aeefea274eccfbaaec9f34c0a4e236f450361b
DaryPet <d7akkord@gmail.com>
2024-12-25 18:00:08 +0100

    fixed fonts

--- src/app/sections/Hero.jsx
+++ src/app/sections/Hero.jsx
@@ -31,7 +31,7 @@ import { PerspectiveCamera } from "@react-three/drei";
 // import ReactLogo from "../components/ReactLogo.jsx";
 // import Button from "../components/Button.jsx";
 // import Target from "../components/Target.jsx";
-// import CanvasLoader from "../components/Loading.jsx";
+import CanvasLoader from "../components/Loader";
 // import HeroCamera from "../components/HeroCamera.jsx";
 import { calculateSizes } from "../constants/index.js";
 import HackerRoom from "../components/HackerRoom.jsx";
@@ -48,7 +48,8 @@ const Hero = () => {
     <section className="min-h-screen w-full flex flex-col relative" id="home">
       <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
         <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
-          Hi, I am Adrian <span className="waving-hand">👋</span>
+          {/* Darya Petrenko <span className="waving-hand">👋</span> */}
+          Darya Petrenko
         </p>
         <p className="hero_tag text-gray_gradient">
           Building Products & Brands
@@ -57,18 +58,18 @@ const Hero = () => {
 
       <div className="w-full h-full absolute inset-0">
         <Canvas className="w-full h-full">
-          {/* <Suspense fallback={<CanvasLoader />}>
+          <Suspense fallback={<CanvasLoader />}>
             {/* To hide controller */}
-          {/* <Leva hidden />  */}
-          <PerspectiveCamera makeDefault position={[0, 0, 30]} />
-          {/* <HeroCamera isMobile={isMobile}> */}
-          <HackerRoom
-          // scale={sizes.deskScale}
-          // position={sizes.deskPosition}
-          // rotation={[0.1, -Math.PI, 0]}
-          />
-          {/* </HeroCamera> */}
-          {/* <group>
+            {/* <Leva hidden />  */}
+            <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+            {/* <HeroCamera isMobile={isMobile}> */}
+            <HackerRoom
+            // scale={sizes.deskScale}
+            // position={sizes.deskPosition}
+            // rotation={[0.1, -Math.PI, 0]}
+            />
+            {/* </HeroCamera> */}
+            {/* <group>
               <Target position={sizes.targetPosition} />
               <ReactLogo position={sizes.reactLogoPosition} />
               <Rings position={sizes.ringPosition} />
@@ -78,6 +79,7 @@ const Hero = () => {
             <ambientLight intensity={1} />
             <directionalLight position={[10, 10, 10]} intensity={0.5} />
           </Suspense> */}
+          </Suspense>
         </Canvas>
       </div>
 
