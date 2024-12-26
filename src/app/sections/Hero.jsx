6d5b16f5918ff05f4538543930f2e9103d041e61
DaryPet <d7akkord@gmail.com>
2024-12-26 18:40:06 +0100

    about-black

--- src/app/sections/Hero.jsx
+++ src/app/sections/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
   return (
     <section className="min-h-screen w-full flex flex-col relative" id="home">
       <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
-        <p className="md:text-3xl text-xl font-medium text-black text-center font-generalsans">
+        <p className="md:text-3xl text-xl font-medium text-white text-center font-generalsans">
           <span className="waving-hand">👋</span> Welcome to my world
         </p>
         <p className="hero_tag text-gray_gradient">
