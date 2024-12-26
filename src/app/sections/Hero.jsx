47758f4e11e5765ee8905055fe8fe0b8cefc1a22
DaryPet <d7akkord@gmail.com>
2024-12-26 11:12:44 +0100

    hero-finished

--- src/app/sections/Hero.jsx
+++ src/app/sections/Hero.jsx
@@ -27,12 +27,11 @@ import { useMediaQuery } from "react-responsive";
 import { PerspectiveCamera } from "@react-three/drei";
 
 // import Cube from "../components/Cube.jsx";
-// import Rings from "../components/Rings.jsx";
+import Rings from "../components/Rings.jsx";
 import ReactLogo from "../components/ReactLogo";
-// import Button from "../components/Button.jsx";
-// import Target from "../components/Target.jsx";
+import Button from "../components/Button";
 import CanvasLoader from "../components/Loader";
-// import HeroCamera from "../components/HeroCamera.jsx";
+import HeroCamera from "../components/HeroCamera";
 import { calculateSizes } from "../constants/index.js";
 import HackerRoom from "../components/HackerRoom.jsx";
 import Arrow from "../components/Arrow";
@@ -56,7 +55,7 @@ const Hero = () => {
   const sizes = calculateSizes(isSmall, isMobile, isTablet);
   return (
     <section className="min-h-screen w-full flex flex-col relative" id="home">
-      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3 ">
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
         <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
           {/* Darya Petrenko <span className="waving-hand">👋</span> */}
           Darya Petrenko
@@ -72,13 +71,13 @@ const Hero = () => {
             {/* To hide controller */}
             {/* <Leva hidden />  */}
             <PerspectiveCamera makeDefault position={[0, 0, 20]} />
-            {/* <HeroCamera isMobile={isMobile}> */}
-            <HackerRoom
-              scale={sizes.deskScale}
-              position={sizes.deskPosition}
-              rotation={[0.1, -Math.PI, 0]}
-            />
-            {/* </HeroCamera> */}
+            <HeroCamera isMobile={isMobile}>
+              <HackerRoom
+                scale={sizes.deskScale}
+                position={sizes.deskPosition}
+                rotation={[0.1, -Math.PI, 0]}
+              />
+            </HeroCamera>
             {/* <group>
               <Target position={sizes.targetPosition} />
               <ReactLogo position={sizes.reactLogoPosition} />
@@ -89,6 +88,7 @@ const Hero = () => {
               <Target position={sizes.targetPosition} />
               <Arrow position={sizes.arrowPosition} />
               <ReactLogo position={sizes.reactLogoPosition} />
+              <Rings position={sizes.ringPosition} />
             </group>
             <ambientLight intensity={1} />
             <directionalLight position={[10, 10, 10]} intensity={0.5} />
@@ -96,15 +96,15 @@ const Hero = () => {
         </Canvas>
       </div>
 
-      {/* <div className="absolute bottom-7 left-0 right-0 w-full z-10 c-space">
-        <a href="#about" className="w-fit">
+      <div className="absolute  bottom-0 left-0 right-0 w-full z-10 c-space">
+        <a href="#contact" className="w-fit">
           <Button
             name="Let's work together"
             isBeam
             containerClass="sm:w-fit w-full sm:min-w-96"
           />
         </a>
-      </div> */}
+      </div>
     </section>
   );
 };
