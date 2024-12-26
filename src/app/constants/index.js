cc787a84df78e5342c22d4c79b891cb4e62aa14a
DaryPet <d7akkord@gmail.com>
2024-12-26 15:30:59 +0100

    positions-fixed all

--- src/app/constants/index.js
+++ src/app/constants/index.js
@@ -253,31 +253,31 @@ export const calculateSizes = (isSmall, isMobile, isTablet) => {
     arrowPosition: isSmall
       ? [2.7, -8, 0]
       : isMobile
-      ? [2.7, -8, 0]
+      ? [3.5, -8, 0]
       : isTablet
-      ? [5, -8, 0]
-      : [5, -8, 0],
+      ? [5, -7.5, 0]
+      : [5, -7, 0],
     reactLogoPosition: isSmall
-      ? [3, 4, 0]
+      ? [2.5, 4, 0]
       : isMobile
-      ? [3, 4, 0]
+      ? [3, 3, 0]
       : isTablet
-      ? [5, 3, 0]
-      : [9, 3, 0],
+      ? [5, 1, 0]
+      : [9, 2, 0],
     ringPosition: isSmall
       ? [-5, 10, 0]
       : isMobile
-      ? [-7, 10, 0]
+      ? [-10, 9, 0]
       : isTablet
-      ? [-12, 8, 2]
-      : [-24, 10, 0],
+      ? [-15, 4, 2]
+      : [-24, 6, 0],
     targetPosition: isSmall
       ? [-4, -10, -7]
       : isMobile
-      ? [-5, -10, -7]
+      ? [-5, -10, -6.5]
       : isTablet
-      ? [-8, -10, -7]
-      : [-9, -13, -13],
+      ? [-8, -10, -8]
+      : [-9, -10, -8],
   };
 };
 
